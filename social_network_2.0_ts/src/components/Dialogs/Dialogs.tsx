b8643a9c3c5dbb880e74c08a96977c69bf0233ea
DmitryShastel <dmshastel@gmail.com>
2022-11-01 14:55:27 +0300

    The social network 1-21 is done

--- social_network_2.0_ts/src/components/Dialogs/Dialogs.tsx
+++ social_network_2.0_ts/src/components/Dialogs/Dialogs.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div>Dima</div>
-                <div>Dima</div>
-                <div>Dima</div>
+                <NavLink to='/dialogs' className={navData => navData.isActive ? s.active : s.item}>Dima</NavLink>
+                <NavLink to='/dialogs' className={navData => navData.isActive ? s.active : s.item}>Dima</NavLink>
+
             </div>
             <div className={s.message}>
                 <div>hi</div>
-                <div>hi</div>
-                <div>hi</div>
+
             </div>
 
         </div>
