78b4ae1ff9b5c30d94698ca474f1fdc38b5b1e27
DmitryShastel <dmshastel@gmail.com>
2022-11-23 09:23:25 +0300

    The social network 1-25 the messagesElements is created

--- social_network_2.0_ts/src/components/Dialogs/Dialogs.tsx
+++ social_network_2.0_ts/src/components/Dialogs/Dialogs.tsx
@@ -14,13 +14,13 @@ type MessageType = {
 
 const DialogItem = (props: DialogItemType) => {
     let path = '/dialogs/' + props.id
-    return(
+    return (
         <NavLink to={path} className={navData => navData.isActive ? s.active : s.item}>{props.name}</NavLink>
     )
 }
 
 const Message = (props: MessageType) => {
-    return(
+    return (
         <div>{props.message}</div>
     )
 }
@@ -39,22 +39,17 @@ export const Dialogs = () => {
         {id: 3, message: 'buy now'}
     ]
 
-    let dialogsElements= dialogs.map(d => <DialogItem name={d.name} id ={d.id}/>)
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+    let messagesElements = messages.map(m => <Message id={m.id} message={m.message}/>)
 
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 {dialogsElements}
-               {/*<DialogItem name={dialogData[0].name} id ={dialogData[0].id}/>*/}
-               {/*<DialogItem name={dialogData[1].name} id ={dialogData[1].id}/>*/}
-               {/*<DialogItem name={dialogData[2].name} id ={dialogData[2].id}/>*/}
             </div>
 
             <div className={s.message}>
-                {/*<Message id={messageData[0].id} message={messageData[0].message}/>*/}
-                {/*<Message id={messageData[1].id} message={messageData[1].message}/>*/}
-                {/*<Message id={messageData[2].id} message={messageData[2].message}/>*/}
-
+                {messagesElements}
             </div>
 
         </div>
