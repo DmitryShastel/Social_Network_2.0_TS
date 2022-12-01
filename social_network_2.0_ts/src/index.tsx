cdf89aa0e5a8157c60e4f0cfc3c8a8b65760dbc7
DmitryShastel <dmshastel@gmail.com>
2022-12-01 18:01:18 +0300

    The social network 1-29 the index is changed

--- social_network_2.0_ts/src/index.tsx
+++ social_network_2.0_ts/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {state} from "./redux/state";
 
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App posts={posts} messages={messages} dialogs={dialogs}/>
+    <App state={state}/>
   </React.StrictMode>
 );
 
