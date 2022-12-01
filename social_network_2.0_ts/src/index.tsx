22480a4576e1a8bd19ee8d51c78dbdc73c2aa3e1
DmitryShastel <dmshastel@gmail.com>
2022-12-01 17:59:18 +0300

    The social network 1-29 the state is created

--- social_network_2.0_ts/src/index.tsx
+++ social_network_2.0_ts/src/index.tsx
@@ -5,24 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
-let posts = [
-    {id: 1, message: 'Hi how are you', like: 15},
-    {id: 2, message: 'It is my first post', like: 23}
-]
-
-let dialogs = [
-    {id: 1, name: 'Dima'},
-    {id: 2, name: 'Sasha'},
-    {id: 3, name: 'Alex'}
-]
-
-let messages = [
-    {id: 1, message: 'hi'},
-    {id: 2, message: 'buy'},
-    {id: 3, message: 'buy now'}
-]
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
---
+++ social_network_2.0_ts/src/redux/state.tsx
@@ -0,0 +1,21 @@
+export let state = {
+    prifilePage: {
+        posts: [
+            {id: 1, message: 'Hi how are you', like: 15},
+            {id: 2, message: 'It is my first post', like: 23}]
+    },
+
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dima'},
+            {id: 2, name: 'Sasha'},
+            {id: 3, name: 'Alex'}
+        ],
+
+        messages: [
+            {id: 1, message: 'hi'},
+            {id: 2, message: 'buy'},
+            {id: 3, message: 'buy now'}
+        ]
+    }
+}
