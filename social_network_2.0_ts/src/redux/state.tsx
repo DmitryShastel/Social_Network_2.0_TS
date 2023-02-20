6f6b184e9f2b1d54208f2485031c0ca341b9df04
DmitryShastel <dmshastel@gmail.com>
2023-02-20 15:52:00 +0300

    The social network 1-37 is done

--- social_network_2.0_ts/src/redux/state.tsx
+++ social_network_2.0_ts/src/redux/state.tsx
@@ -28,52 +28,6 @@ export type ProfilePageType = {
 export type addPostType = () => void
 export type updateNewPostTextType = (newPostText: string) => void
 
-// let renderEntireTree = (state: StateType) => {
-//
-// }
-// export let addPost = () => {
-//     let newPost: PostsType =
-//         {
-//             id: new Date().getTime(),
-//             message: state.profilePage.newPostText,
-//             like: 0
-//         }
-//     state.profilePage.posts.push(newPost)
-//     state.profilePage.newPostText = ''
-//     renderEntireTree(state)
-// }
-// export let updateNewPostText = (newPostText: string) => {
-//     state.profilePage.newPostText = newPostText
-//     renderEntireTree(state)
-// }
-
-//export let subscribe = (observer: any) => {renderEntireTree = observer}
-
-
-// export let state: StateType = {
-//     profilePage: {
-//         posts: [
-//             {id: 1, message: 'Hi how are you', like: 15},
-//             {id: 2, message: 'It is my first post', like: 23}
-//         ],
-//         newPostText: '',
-//     },
-//
-//
-//     dialogsPage: {
-//         dialogs: [
-//             {id: 1, name: 'Dima'},
-//             {id: 2, name: 'Sasha'},
-//             {id: 3, name: 'Alex'}
-//         ],
-//
-//         messages: [
-//             {id: 1, message: 'hi'},
-//             {id: 2, message: 'buy'},
-//             {id: 3, message: 'buy now'}
-//         ]
-//     }
-// }
 
 export let store = {
     _state: {
@@ -103,7 +57,7 @@ export let store = {
     getState() {
         return this._state
     },
-   callSubscriber () {},
+   _callSubscriber (state: StateType) {},
     addPost () {
         let newPost: PostsType =
             {
@@ -113,13 +67,13 @@ export let store = {
             };
         this._state.profilePage.posts.push(newPost)
         this._state.profilePage.newPostText = ''
-        this.callSubscriber(this._state)
+        this._callSubscriber(this._state)
     },
     updateNewPostText(newPostText: string) {
         this._state.profilePage.newPostText = newPostText
-        this.callSubscriber(this._state)
+        this._callSubscriber(this._state)
     },
     subscribe (abserver: any) {
-        this.callSubscriber = abserver
+        this._callSubscriber = abserver
     }
 }
