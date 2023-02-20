66d8d3a1cbca5c4894754b2d196f9407b107a2e3
DmitryShastel <dmshastel@gmail.com>
2023-02-20 15:06:27 +0300

    The social network 1-37 store is created

--- social_network_2.0_ts/src/redux/state.tsx
+++ social_network_2.0_ts/src/redux/state.tsx
@@ -15,14 +15,11 @@ export type dialogsPageType = {
     dialogs: Array<DialogsType>
     messages: Array<MessageType>
 }
-
 export type PostsType = {
     id: number
     message: string
     like: number
 }
-
-
 export type ProfilePageType = {
     posts: Array<PostsType>
     newPostText: string
@@ -55,27 +52,57 @@ export let subscribe = (observer: any) => {
 }
 
 
-export let state: StateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi how are you', like: 15},
-            {id: 2, message: 'It is my first post', like: 23}
-        ],
-        newPostText: '',
+// export let state: StateType = {
+//     profilePage: {
+//         posts: [
+//             {id: 1, message: 'Hi how are you', like: 15},
+//             {id: 2, message: 'It is my first post', like: 23}
+//         ],
+//         newPostText: '',
+//     },
+//
+//
+//     dialogsPage: {
+//         dialogs: [
+//             {id: 1, name: 'Dima'},
+//             {id: 2, name: 'Sasha'},
+//             {id: 3, name: 'Alex'}
+//         ],
+//
+//         messages: [
+//             {id: 1, message: 'hi'},
+//             {id: 2, message: 'buy'},
+//             {id: 3, message: 'buy now'}
+//         ]
+//     }
+// }
+
+export let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi how are you', like: 15},
+                {id: 2, message: 'It is my first post', like: 23}
+            ],
+            newPostText: '',
+        },
+
+
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Sasha'},
+                {id: 3, name: 'Alex'}
+            ],
+
+            messages: [
+                {id: 1, message: 'hi'},
+                {id: 2, message: 'buy'},
+                {id: 3, message: 'buy now'}
+            ]
+        }
     },
-
-
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Sasha'},
-            {id: 3, name: 'Alex'}
-        ],
-
-        messages: [
-            {id: 1, message: 'hi'},
-            {id: 2, message: 'buy'},
-            {id: 3, message: 'buy now'}
-        ]
-    }
+    getState() {
+        return this._state
+    },
 }
