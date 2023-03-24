1fe57528cbedbafd3be864bb344e9063c9921760
DmitryShastel <dmshastel@gmail.com>
2023-03-24 16:02:55 +0300

    The social-network 1-44 MyPostsContainer is changed

--- social_network_2.0_ts/src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ social_network_2.0_ts/src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -11,27 +11,27 @@ type MyPostsContainerType = {
 
 export const MyPostsContainer = (props: MyPostsContainerType) => {
 
-    let state = props.store.getState()
-
-    const addPost = () => {
-        props.store.dispatch(addNewPostTextActionCreator())
-    }
-
-    const onPostChange = (text: string) => {
-        let action = updateNewPostTextActionCreator(text)
-        props.store.dispatch(action)
-    }
-
-
     return (
         <StoreContext.Consumer>{
-            (store) => (
-                <MyPosts
+            (store) => {
+
+                let state = props.store.getState()
+
+                const addPost = () => {
+                    props.store.dispatch(addNewPostTextActionCreator())
+                }
+
+                const onPostChange = (text: string) => {
+                    let action = updateNewPostTextActionCreator(text)
+                    props.store.dispatch(action)
+                }
+
+                return <MyPosts
                     posts={state.profilePage.posts}
                     newPostText={state.profilePage.newPostText}
                     updateNewPostText={onPostChange}
                     addPost={addPost}/>
-            )
+            }
         }
         </StoreContext.Consumer>
 
