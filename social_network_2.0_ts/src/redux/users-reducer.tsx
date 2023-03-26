8f25ebf3a86d5f180db5a35e2c7e6550dae29e7f
DmitryShastel <dmshastel@gmail.com>
2023-03-26 14:25:51 +0300

    The social-network 1-49 FOLLOW AC is created

--- social_network_2.0_ts/src/redux/users-reducer.tsx
+++ social_network_2.0_ts/src/redux/users-reducer.tsx
@@ -1,28 +1,13 @@
-const SEND_MESSAGE = 'SEND-MESSAGE'
-const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'
+const FOLLOW = 'FOLLOW'
 
-export type AddMessageActionType = {
-    type: 'SEND-MESSAGE'
-}
-export type UpdateMessageActionType = {
-    type: 'UPDATE-NEW-MESSAGE-TEXT',
-    body: string
+export type FollowACType = (userId: any) => FollowActionType
+
+export type FollowActionType = {
+    type: 'FOLLOW',
+    userId: number
 }
 
-export type SendMessageActionCreatorType = () => AddMessageActionType
-export type UpdateMessageActionCreatorType = (body: string) => UpdateMessageActionType
-
-export type ActionType = AddMessageActionType | UpdateMessageActionType
-
-
-export type MessageType = {
-    id: number
-    message: string
-}
-export type DialogsType = {
-    id: number
-    name: string
-}
+export type ActionType = FollowActionType | UpdateMessageActionType
 
 let initialState = {
    users: [
@@ -41,6 +26,18 @@ export type InitialStateType = typeof initialState
 
 export const dialogsReducer = (state: InitialStateType = initialState, action: ActionType): InitialStateType => {
     switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(u => {
+                    if(u.id === action.userId) {
+                        return {
+                            ...u, followed: true
+                        }
+                    }
+                    return u
+                })
+            }
 
 
         default:
@@ -48,9 +45,10 @@ export const dialogsReducer = (state: InitialStateType = initialState, action: A
     }
 }
 
-export const sendMessageActionCreator: SendMessageActionCreatorType = () => {
+export const followAC: FollowACType = (userId: any) => {
     return {
-        type: SEND_MESSAGE
+        type: FOLLOW,
+        userId
     }
 }
 export const updateNewMessageBodyActionCreator: UpdateMessageActionCreatorType = (body: string) => {
@@ -58,4 +56,28 @@ export const updateNewMessageBodyActionCreator: UpdateMessageActionCreatorType =
         type: UPDATE_NEW_MESSAGE_TEXT,
         body: body
     }
+}
+
+
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'
+
+
+export type UpdateMessageActionType = {
+    type: 'UPDATE-NEW-MESSAGE-TEXT',
+    body: string
+}
+
+
+export type UpdateMessageActionCreatorType = (body: string) => UpdateMessageActionType
+
+
+
+
+export type MessageType = {
+    id: number
+    message: string
+}
+export type DialogsType = {
+    id: number
+    name: string
 }
